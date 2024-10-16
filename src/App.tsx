6014cc157f0bf12b31e6a96210a117c2bca5b0a3
o-su <xmbvztcxzc@email.cz>
2024-10-16 11:37:24 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      Zde bude formulář <i>form/MainForm.tsx</i>:
+    </>
+  );
+}
+
+export default App;
---
+++ src/form/MainForm.tsx
@@ -0,0 +1,35 @@
+/**
+ * Zde vytvořte formulář pomocí knihovny react-hook-form.
+ * Formulář by měl splňovat:
+ * 1) být validován yup schématem
+ * 2) formulář obsahovat pole "NestedFields" z jiného souboru
+ * 3) být plně TS typovaný
+ * 4) nevalidní vstupy červeně označit (background/outline/border) a zobrazit u nich chybové hlášky
+ * 5) nastavte výchozí hodnoty objektem initalValues
+ * 6) mít "Submit" tlačítko, po jeho stisku se vylogují data z formuláře:  "console.log(formData)"
+ *
+ * V tomto souboru budou definovány pole:
+ * amount - number; Validace min=0, max=300
+ * damagedParts - string[] formou multi-checkboxu s volbami "roof", "front", "side", "rear"
+ * vykresleny pole z form/NestedFields
+ */
+
+// příklad očekávaného výstupního JSON, předvyplňte tímto objektem formulář
+const initialValues = {
+  amount: 250,
+  allocation: 140,
+  damagedParts: ['side', 'rear'],
+  category: 'kitchen-accessories',
+  witnesses: [
+    {
+      name: 'Marek',
+      email: 'marek@email.cz',
+    },
+    {
+      name: 'Emily',
+      email: 'emily.johnson@x.dummyjson.com',
+    },
+  ],
+};
+
+const damagedPartsOptions = ['roof', 'front', 'side', 'rear'];
